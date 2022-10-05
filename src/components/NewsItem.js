0c39539eac550e105fc7e4bc5c0de888d78e1067
amit chauhan <amitharidwarchauhan@gmail.com>
2022-10-05 20:37:02 +0530

    Add Top News Api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,9 +8,9 @@ export class NewsItem extends Component {
                 <div className="card" style={{width: "18rem"}}>
                     <img src={imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
